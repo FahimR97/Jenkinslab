e10ff076c8c76f16085607ddbea5dcefdc3f30f1
Ubuntu <ubuntu@ip-172-31-39-202.eu-west-2.compute.internal>
2025-03-27 14:31:09 +0000

    Updated nginx config and app port

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def home():
     return f"Hello! I'm currently running in {socket.gethostname()}"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)  # Changed port to 5000
